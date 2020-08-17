4fe8463c153673463a09cd91d608d8d0956c8539
sinnna <sharifi.sina1377@gmail.com>
2020-08-17 21:14:55 +0430

    2 sec delay added so manager has time to get topology. also pingall for multuple times are added, need to change pingall to tcp connection

--- new_dtopo.py
+++ new_dtopo.py
@@ -8,54 +8,62 @@ import time
 
 c0 = RemoteController( 'c0', ip='127.0.0.1', port=6633 )
 
-topo = Topo()  # Create an empty topology
+# 5
+num_runs = 1
+# 6
+num_net_up = 2
 
-# Add hosts
-h1 = topo.addHost( 'h1' )
-h2 = topo.addHost( 'h2' )
-h3 = topo.addHost( 'h3' )
-h4 = topo.addHost( 'h4' )
-h5 = topo.addHost( 'h5' )
-h6 = topo.addHost( 'h6' )
-h7 = topo.addHost( 'h7' )
+for i in range(num_runs):
+	for j in range(num_net_up):
 
-# Add switches
-s1 = topo.addSwitch( 's1' )
-s2 = topo.addSwitch( 's2' )
-s3 = topo.addSwitch( 's3' )
-s4 = topo.addSwitch( 's4' )
+		topo = Topo()  # Create an empty topology
 
-# Add links
-bw = random.randint(0, 4) + 1
-topo.addLink('h1', 's1', bw=bw, use_htb=True, cls=TCLink)
-bw = random.randint(0, 4) + 1
-topo.addLink('h2', 's2', bw=bw, use_htb=True, cls=TCLink)
-bw = random.randint(0, 4) + 1
-topo.addLink('h3', 's3', bw=bw, use_htb=True, cls=TCLink)
-bw = random.randint(0, 4) + 1
-topo.addLink('h4', 's3', bw=bw, use_htb=True, cls=TCLink)
-bw = random.randint(0, 4) + 1
-topo.addLink('h5', 's4', bw=bw, use_htb=True, cls=TCLink)
-bw = random.randint(0, 4) + 1
-topo.addLink('h6', 's4', bw=bw, use_htb=True, cls=TCLink)
-bw = random.randint(0, 4) + 1
-topo.addLink('h7', 's4', bw=bw, use_htb=True, cls=TCLink)
+		# Add hosts
+		h1 = topo.addHost( 'h1' )
+		h2 = topo.addHost( 'h2' )
+		h3 = topo.addHost( 'h3' )
+		h4 = topo.addHost( 'h4' )
+		h5 = topo.addHost( 'h5' )
+		h6 = topo.addHost( 'h6' )
+		h7 = topo.addHost( 'h7' )
 
-bw = random.randint(0, 4) + 1
-topo.addLink('s1', 's2', bw=bw, use_htb=True, cls=TCLink)
-bw = random.randint(0, 4) + 1
-topo.addLink('s2', 's3', bw=bw, use_htb=True, cls=TCLink)
-# topo.addLink('s1', 's3')
-bw = random.randint(0, 4) + 1
-topo.addLink('s3', 's4', bw=bw, use_htb=True, cls=TCLink)
-# topo.addLink('s2', 's4')
+		# Add switches
+		s1 = topo.addSwitch( 's1' )
+		s2 = topo.addSwitch( 's2' )
+		s3 = topo.addSwitch( 's3' )
+		s4 = topo.addSwitch( 's4' )
+
+		# Add links
+		bw = random.randint(0, 4) + 1
+		topo.addLink('h1', 's1', bw=bw, use_htb=True, cls=TCLink)
+		bw = random.randint(0, 4) + 1
+		topo.addLink('h2', 's2', bw=bw, use_htb=True, cls=TCLink)
+		bw = random.randint(0, 4) + 1
+		topo.addLink('h3', 's3', bw=bw, use_htb=True, cls=TCLink)
+		bw = random.randint(0, 4) + 1
+		topo.addLink('h4', 's3', bw=bw, use_htb=True, cls=TCLink)
+		bw = random.randint(0, 4) + 1
+		topo.addLink('h5', 's4', bw=bw, use_htb=True, cls=TCLink)
+		bw = random.randint(0, 4) + 1
+		topo.addLink('h6', 's4', bw=bw, use_htb=True, cls=TCLink)
+		bw = random.randint(0, 4) + 1
+		topo.addLink('h7', 's4', bw=bw, use_htb=True, cls=TCLink)
+
+		bw = random.randint(0, 4) + 1
+		topo.addLink('s1', 's2', bw=bw, use_htb=True, cls=TCLink)
+		bw = random.randint(0, 4) + 1
+		topo.addLink('s2', 's3', bw=bw, use_htb=True, cls=TCLink)
+		# topo.addLink('s1', 's3')
+		bw = random.randint(0, 4) + 1
+		topo.addLink('s3', 's4', bw=bw, use_htb=True, cls=TCLink)
+		# topo.addLink('s2', 's4')
 
 
-net = Mininet(topo=topo, controller=c0)
-net.start()
+		net = Mininet(topo=topo, controller=c0)
+		net.start()
 
-net.pingAll()
-net.pingAll()
+		time.sleep(2)
+		net.pingAll()
 
-# CLI(net)  # Bring up the mininet CLI
-net.stop()
+		# CLI(net)  # Bring up the mininet CLI
+		net.stop()	
